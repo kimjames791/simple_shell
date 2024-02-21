35c38cf0c280bee311d1c8d1bb58c4979ad8db6a
kimjames791 <kimjames791@gmail.com>
2024-02-21 11:15:01 -0800

    prompt.c

---
+++ prompt.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * shows_prompt - Displays the shell prompt
+ *
+ * This function displays the shell prompt whenever the shell is initiated.
+ * The prompt is typically in the format "kimtopshell$".
+ */
+void shows_prompt(void)
+{
+	rkmprint_func("kimtopshell$");
+}
